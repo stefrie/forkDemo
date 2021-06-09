04937297517128e22946d45b375ff50a27c35683
SARA RUSSERT <sarabeth.russert@gmail.com>
2021-06-09 14:13:46 -0700

    preps for class

--- functions.js
+++ functions.js
@@ -1,21 +1,19 @@
 'use strict';
 
-//Challenge 1
+// -------- Challenge 1 -------- //
 // put this code block into a function, add a return statement, and call the function
 
-function bestPetsFunction() {
-  let bestPet = 'dog';
-  let petName = 'indiana'
-  let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
-  // return the petString here
-  return petString;
-}
+let bestPet = 'dog';
+let petName = 'indiana'
+let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
+// return the petString here
+
 
 // call your function here
-bestPetsFunction();
 
 
-//Challenge 2
+
+// -------- Challenge 2 -------- //
 // put this code block into a function, add a return statement, and call the function
 
 let anotherGreatPet = 'bunny';
@@ -27,7 +25,7 @@ let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anot
 
 
 
-// Challenge 3
+// -------- Challenge 3 -------- //
 // is there another way to write this function so we can use one function for different pets?
 
 
