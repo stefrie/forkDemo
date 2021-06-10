d4885d094fee4dc39936f634b1cfbc145d9e924a
Chance Harmon <henrikillsit@gmail.com>
2021-06-10 11:24:39 -0700

    fork demo with Sara

--- functions.js
+++ functions.js
@@ -2,15 +2,18 @@
 
 // -------- Challenge 1 -------- //
 // put this code block into a function, add a return statement, and call the function
+function funFunction() {
 
-let bestPet = 'dog';
-let petName = 'indiana'
-let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
-// return the petString here
+    let bestPet = 'dog';
+    let petName = 'indiana'
+    let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
+    // return the petString here
+    return petString;
+}
 
 
 // call your function here
-
+funFunction();
 
 
 // -------- Challenge 2 -------- //
